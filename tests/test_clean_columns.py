218da48ddc5d97e9139c898497b09979c5052b42
bharath <you@example.com>
2024-02-24 13:05:55 +0000

    adding pytest module and initial tests

---
+++ tests/test_clean_columns.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("..")
+
+from transform import *
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+object_transform = Transform()
+
+def test_clean_column_names():
+    data = [['123', '456']]
+    input_df = pd.DataFrame(data, columns=['Transaction No', 'Product No'])
+    output_df = pd.DataFrame(data, columns=['Transaction_No', 'Product_No'])
+    assert_frame_equal(object_transform.clean_columns(input_df), output_df)
---
+++ tests/test_standardize_columns.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("..")
+
+from transform import Transform
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+object_transform = Transform()
+
+def test_standardize_columns():
+    data = [['123', '456']]
+    input_df = pd.DataFrame(data, columns=['Transaction_No', 'Product_No'])
+    output_df = pd.DataFrame(data, columns=['transaction_id', 'product_id'])
+    assert_frame_equal(object_transform.standardize_columns(input_df), output_df)
