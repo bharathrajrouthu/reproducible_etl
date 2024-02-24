2faddee77eee1009a0d92372b5e7ae9a575aed2f
bharath <you@example.com>
2024-02-24 02:51:16 +0000

    cleaning working tree functions load

--- load.py
+++ load.py
@@ -9,54 +9,6 @@ class Load:
     '''
     connection to DB and data insertion
     '''
-    def create_tables(self, df):
-        '''
-        Creates databse tables.
-        Parameters:
-            self: The class instance
-            df: Input DataFrame.
-        Returns:
-            sales_dataframe: DataFrame containing sales data.
-            customer_dataframe: DataFrame containing customer data.
-            unique_product_df: DataFrame containing unique product data.
-            transaction_dataframe: DataFrame containing transaction data.
-            country_dataframe: DataFrame containing country data.
-            date_dataframe: DataFrame containing date-related data.
-        '''
-        #  A new customer dataframe
-        customer_dataframe = df[["customer_id"]].drop_duplicates().sort_values(by="customer_id")
-        customer_dataframe = customer_dataframe.reset_index(drop=True)
-        customer_dataframe["customer_key"] = customer_dataframe.index 
-
-        # A new product dataframe
-        product_dataframe = df[['product_id', "name", "price"]]
-        unique_product_df = product_dataframe.drop_duplicates().sort_values(by=['name', 'price']).reset_index(drop=True)
-        unique_product_df['product_key'] = unique_product_df.index
-        unique_product_df = product_dataframe.merge(unique_product_df, on=['product_id', 'name', 'price']).sort_values(by=['name', 'price']).drop_duplicates()
-
-        # A new transaction dataframe
-        transaction_dataframe = df[["transaction_id"]].drop_duplicates().sort_values(by="transaction_id")
-        transaction_dataframe = transaction_dataframe.reset_index(drop=True)
-        transaction_dataframe["transaction_key"] = transaction_dataframe.index
-
-        # # A new country dataframe
-        country_dataframe = df[["country", "country_id"] ].drop_duplicates().sort_values(by="country_id")
-        country_dataframe = country_dataframe.reset_index(drop=True)
-        country_dataframe["country_key"] = country_dataframe.index
-
-        # # A new date dataframe
-        date_dataframe = df[["date", "week", "day_name",  "day", "month", "quarter", "year"]].drop_duplicates().sort_values(by="date")
-        date_dataframe = date_dataframe.reset_index(drop=True)
-        date_dataframe["date_key"] = date_dataframe.index
-        
-        # Aggregate the results for sales dataframe
-        result = df.merge(customer_dataframe, on="customer_id")
-        result = result.merge(unique_product_df, on=["product_id", "name", "price"])
-        result = result.merge(transaction_dataframe, on="transaction_id")
-        result = result.merge(country_dataframe, on=["country_id", "country"])
-        sales_dataframe = result.merge(date_dataframe, on=["date", "week", "day_name",  "day", "month", "quarter", "year"])
-
-        return sales_dataframe, customer_dataframe, unique_product_df, transaction_dataframe, country_dataframe, date_dataframe
 
     def insert_tables(self, sales_dataframe, customer_dataframe, unique_product_df, transaction_dataframe, country_dataframe, date_dataframe):
         '''
@@ -113,7 +65,7 @@ class Load:
         with engine.connect() as conn:
             conn.execute(text("ALTER TABLE date ADD PRIMARY KEY (date_key);"))
 
-        print("**INSERTION COMPLETED**")
+        print("**INSERTION COMPLETED")
         log("Loaded Data into DB")
 
         '''
