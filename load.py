4608cd8a6c966951f134894aacd01bc402af4f6b
bharath <you@example.com>
2024-02-24 02:45:37 +0000

    testing function in test.py

--- load.py
+++ load.py
@@ -1,13 +1,199 @@
-def load_csv(self, path: str):
-    '''
-    Load to a csv file
-    :param path: Path to the csv file to load data into
-    '''
-    self.data.to_csv(path)
+from logs.log import log
+from sqlalchemy import create_engine, text
+import pandas as pd
 
-def load_pickle(self, path):
+class Load:
+
+    def __init__(self):
+        pass
     '''
-    Load to an pkl file
-    :param path: Path to the pkl file to load data into
+    connection to DB and data insertion
     '''
-    self.data.to_pickle(path)
+    def create_tables(self, df):
+        '''
+        Creates databse tables.
+        Parameters:
+            self: The class instance
+            df: Input DataFrame.
+        Returns:
+            sales_dataframe: DataFrame containing sales data.
+            customer_dataframe: DataFrame containing customer data.
+            unique_product_df: DataFrame containing unique product data.
+            transaction_dataframe: DataFrame containing transaction data.
+            country_dataframe: DataFrame containing country data.
+            date_dataframe: DataFrame containing date-related data.
+        '''
+        #  A new customer dataframe
+        customer_dataframe = df[["customer_id"]].drop_duplicates().sort_values(by="customer_id")
+        customer_dataframe = customer_dataframe.reset_index(drop=True)
+        customer_dataframe["customer_key"] = customer_dataframe.index 
+
+        # A new product dataframe
+        product_dataframe = df[['product_id', "name", "price"]]
+        unique_product_df = product_dataframe.drop_duplicates().sort_values(by=['name', 'price']).reset_index(drop=True)
+        unique_product_df['product_key'] = unique_product_df.index
+        unique_product_df = product_dataframe.merge(unique_product_df, on=['product_id', 'name', 'price']).sort_values(by=['name', 'price']).drop_duplicates()
+
+        # A new transaction dataframe
+        transaction_dataframe = df[["transaction_id"]].drop_duplicates().sort_values(by="transaction_id")
+        transaction_dataframe = transaction_dataframe.reset_index(drop=True)
+        transaction_dataframe["transaction_key"] = transaction_dataframe.index
+
+        # # A new country dataframe
+        country_dataframe = df[["country", "country_id"] ].drop_duplicates().sort_values(by="country_id")
+        country_dataframe = country_dataframe.reset_index(drop=True)
+        country_dataframe["country_key"] = country_dataframe.index
+
+        # # A new date dataframe
+        date_dataframe = df[["date", "week", "day_name",  "day", "month", "quarter", "year"]].drop_duplicates().sort_values(by="date")
+        date_dataframe = date_dataframe.reset_index(drop=True)
+        date_dataframe["date_key"] = date_dataframe.index
+        
+        # Aggregate the results for sales dataframe
+        result = df.merge(customer_dataframe, on="customer_id")
+        result = result.merge(unique_product_df, on=["product_id", "name", "price"])
+        result = result.merge(transaction_dataframe, on="transaction_id")
+        result = result.merge(country_dataframe, on=["country_id", "country"])
+        sales_dataframe = result.merge(date_dataframe, on=["date", "week", "day_name",  "day", "month", "quarter", "year"])
+
+        return sales_dataframe, customer_dataframe, unique_product_df, transaction_dataframe, country_dataframe, date_dataframe
+
+    def insert_tables(self, sales_dataframe, customer_dataframe, unique_product_df, transaction_dataframe, country_dataframe, date_dataframe):
+        '''
+        Inserts tables with into database.
+        Parameters:
+            self: The class instance.
+            sales_dataframe: DataFrame containing sales data.
+            customer_dataframe: DataFrame containing customer data.
+            unique_product_df: DataFrame containing unique product data.
+            transaction_dataframe: DataFrame containing transaction data.
+            country_dataframe: DataFrame containing country data.
+            date_dataframe: DataFrame containing date-related data.
+        Returns:
+            None
+        '''
+        engine = create_engine("postgresql://postgres@localhost:5432/onprem_db")
+        engine.connect()
+        log("Connected to the Database")
+        '''
+        INSERTION
+        '''
+        # Sales Table Insertion
+        sales_table = sales_dataframe[["customer_key", "transaction_key", "date_key", "product_key", "country_key", "quantity"]].sort_values(by="date_key")
+        sales_table.to_sql("sales", con=engine, if_exists="replace", index=False)
+
+        # Customer Table Insertion
+        customer_table = customer_dataframe.set_index("customer_key")
+        customer_table.to_sql("customer", con=engine, if_exists="replace")
+        with engine.connect() as conn:
+            conn.execute(text("ALTER TABLE customer ADD PRIMARY KEY (customer_key);"))
+
+        # Product Table Insertion
+        product_table = unique_product_df.set_index("product_key")
+        product_table.to_sql("product", con=engine, if_exists="replace")
+        with engine.connect() as conn:
+            conn.execute(text("ALTER TABLE product ADD PRIMARY KEY (product_key);"))
+
+        # Transaction Table Insertion
+        transaction_table = transaction_dataframe.set_index("transaction_key")
+        transaction_table.to_sql("transaction", con=engine, if_exists="replace")
+        with engine.connect() as conn:
+            conn.execute(text("ALTER TABLE transaction ADD PRIMARY KEY (transaction_key);"))
+
+        # Country Table Insertion
+        country_table = country_dataframe.set_index("country_key")
+        country_table.to_sql("country", con=engine, if_exists="replace")
+        with engine.connect() as conn:
+            conn.execute(text("ALTER TABLE country ADD PRIMARY KEY (country_key);"))
+
+       # Date Table Insertion
+        date_table = date_dataframe.set_index("date_key")
+        date_table['date'] = date_table['date'].dt.date
+        date_table.to_sql("date", con=engine, if_exists="replace")
+        with engine.connect() as conn:
+            conn.execute(text("ALTER TABLE date ADD PRIMARY KEY (date_key);"))
+
+        print("**INSERTION COMPLETED**")
+        log("Loaded Data into DB")
+
+        '''
+        DB schema and function queries to insert into DB
+        '''
+        # try:
+            
+        #     with engine.connect() as conn:
+        #         print("**INSERTION IN PROGRESS**")
+        #         conn.execute(text("""
+        #         ALTER TABLE sales_fact 
+        #         ADD PRIMARY KEY (customer_key, transaction_key, date_key, product_key, country_key);
+        #         """))
+        #         conn.execute(text("""
+        #         ALTER TABLE sales_fact
+        #         ADD CONSTRAINT fk_customer_dim_sales_fact
+        #         FOREIGN KEY (customer_key)
+        #         REFERENCES customer_dim (customer_key);
+        #         """))
+        #         conn.execute(text("""
+        #         ALTER TABLE sales_fact
+        #         ADD CONSTRAINT fk_transaction_dim_sales_fact
+        #         FOREIGN KEY (transaction_key)
+        #         REFERENCES transaction_dim (transaction_key);
+        #         """))
+        #         conn.execute(text("""
+        #         ALTER TABLE sales_fact
+        #         ADD CONSTRAINT fk_date_dim_sales_fact
+        #         FOREIGN KEY (date_key)
+        #         REFERENCES date_dim (date_key);
+        #         """))
+        #         conn.execute(text("""
+        #         ALTER TABLE sales_fact
+        #         ADD CONSTRAINT fk_product_dim_sales_fact
+        #         FOREIGN KEY (product_key)
+        #         REFERENCES product_dim (product_key);
+        #         """))
+        #         conn.execute(text("""
+        #         ALTER TABLE sales_fact
+        #         ADD CONSTRAINT fk_country_dim_sales_fact
+        #         FOREIGN KEY (country_key)
+        #         REFERENCES country_dim (country_key);
+        #         """))
+        #     print("**ALL THE STATEMNETS EXECUTED AND INSERTION DONE**")
+        #     log("Succesfully completed data insertion into DB")
+        # except Exception as e:
+            # print(e)
+
+
+
+
+
+
+
+##########################################################################################################################################################################
+# def load_csv(self, path: str):
+#     '''
+#     Load to a csv file
+#     :param path: Path to the csv file to load data into
+#     '''
+#     self.data.to_csv(path)
+
+# def load_pickle(self, path):
+#     '''
+#     Load to an pkl file
+#     :param path: Path to the pkl file to load data into
+#     '''
+#     self.data.to_pickle(path)
+        
+
+# def load_table(df: pd.DataFrame, table_name: str, key: str, replacement_key: str):
+#     engine = create_engine("postgresql://postgres@localhost:5432/onprem_db")
+#     engine.connect()
+
+#     df_table = df[[key]].drop_duplicates().sort_values(by=key)
+#     df_table = df_table.reset_index(drop=True)
+#     df_table[replacement_key] = df_table.index
+
+#     table_name= df_table.set_index(key)
+#     dim_table_name = f"{table_name}_dim"
+#     table_name.to_sql(dim_table_name, con=engine, if_exists="replace")
+#     with engine.connect() as conn:
+#         conn.execute(text(f"ALTER TABLE {dim_table_name} ADD PRIMARY KEY ({replacement_key});"))
--- main.py
+++ main.py
@@ -1,10 +1,61 @@
-if __name__ == '__main__':
+from extract import Extract
+from transform import Transform
+from load import Load
 
-    # parse args
-    args = parser.parse_args()
+from sqlalchemy import create_engine, text
 
+def main():
+    '''
+    Extract data
+    '''
+    extract = Extract()
+    df = extract.read_data('data/transaction-data.csv')
+    '''
+    Transform the data
+    '''
+    transform = Transform()
+    # Clean column names
+    df = transform.clean_columns(df)
+    # Standardize column names
+    df = transform.standardize_columns(df)
+    # Remove duplicates
+    df = transform.remove_duplicates(df)
+    # Remove missing values
+    df = transform.remove_missing_values(df)
+    # Remove empty orders
+    df = transform.remove_empty_orders(df)
+    # Cast types
+    df = transform.cast_types(df)
+    # Cast data types
+    df = transform.create_columns(df)
+    # the processed dataframe
+    print("** TEST DATA FRAME **")
+    print(df.columns)
+    '''
+    Load data to the Database
+    '''
     try:
-        pass
-    except FileNotFoundError:
-        print()
-        pass
+        load = Load()
+        sales_df, customer_df, product_df, transaction_df, country_df, date_df = load.create_tables(df)
+        load.insert_tables(sales_df, customer_df, product_df, transaction_df, country_df, date_df)
+
+    except Exception as e:
+        print("Error has occured during DB Ingestion: ", e)
+
+if __name__ == '__main__':
+    main()
+
+
+############################################################################################################
+    '''
+    Step 1: Call extract data from extract.py
+    Step 2: Call transform function from transform.py - pass data as argumnet to perform transformations
+    Step 3: Once returned df. Call load from load.py
+    '''
+
+    # data = extract_data()
+    # transform = transform_data(data)
+    '''
+    Try test connection from postgres DB
+    '''
+    # load = load_data(transform)
